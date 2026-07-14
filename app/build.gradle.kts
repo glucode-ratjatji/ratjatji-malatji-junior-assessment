6b181948cad88b71781921b32215a8f4b8911c8d
glucode-ratjatji <ratjatji@glucode.com>
2026-07-14 15:02:29 +0200

    chore: initialise project with compose & gradle depencies for icons, room and networking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    // Add the KSP plugin
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.glucode_getitdone_to_do_list"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -39,6 +41,23 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
+    //Extra icons
+    implementation("androidx.compose.material:material-icons-extended")
+
+    // Networking: Retrofit HTTP client
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+
+    // JSON Parsing: Converts API responses into Kotlin data classes
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    // Concurrency: Kotlin Coroutines for asynchronous API requests
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+
+    val room_version = "2.8.4" // Check for the latest stable version
+
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version") // Optional for Kotlin/Coroutines
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
@@ -46,7 +65,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.room3.common.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
