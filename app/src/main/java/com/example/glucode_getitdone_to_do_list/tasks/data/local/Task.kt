0d4314e36e4fdfe7af82a634eea8df345b69f2e1
glucode-ratjatji <ratjatji@glucode.com>
2026-07-15 11:03:51 +0200

    to fix: Room implementation

---
+++ app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/data/local/Task.kt
@@ -0,0 +1,4 @@
+package com.example.glucode_getitdone_to_do_list.tasks.data.local
+
+class Task {
+}
--- app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/data/local/TaskDao.kt
+++ app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/data/local/TaskDao.kt
@@ -5,20 +5,20 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
-@Dao
-class TaskDao {
-    @Query("SELECT * FROM task")
-    fun getAll(): List<Task>
-
-    @Query("SELECT * FROM task WHERE uid IN (:taskIds)")
-    fun loadAllByIds(taskIds: IntArray): List<Task>
-
-    @Query("SELECT * FROM task WHERE task_title LIKE :title LIMIT 1")
-    fun findByTitle(title: String): Task
-
-    @Insert
-    fun insertAll(vararg task: Task)
-
-    @Delete
-    fun delete(task: Task)
-}
+//@Dao
+//class TaskDao {
+//    @Query("SELECT * FROM task")
+//    fun getAll(): List<Task>
+//
+//    @Query("SELECT * FROM task WHERE uid IN (:taskIds)")
+//    fun loadAllByIds(taskIds: IntArray): List<Task>
+//
+//    @Query("SELECT * FROM task WHERE task_title LIKE :title LIMIT 1")
+//    fun findByTitle(title: String): Task
+//
+//    @Insert
+//    fun insertAll(vararg task: Task)
+//
+//    @Delete
+//    fun delete(task: Task)
+//}
