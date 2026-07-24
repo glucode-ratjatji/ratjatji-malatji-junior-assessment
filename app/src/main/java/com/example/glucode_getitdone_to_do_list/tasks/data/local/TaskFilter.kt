1652f48015bb6906458f6fae61b7646766b5db85
glucode-ratjatji <ratjatji@glucode.com>
2026-07-24 11:06:22 +0200

    feat: filtering works with ToDoTab

---
+++ app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/data/local/TaskFilter.kt
@@ -0,0 +1,6 @@
+package com.example.glucode_getitdone_to_do_list.tasks.data.local
+
+enum class TaskFilter(val label: String) {
+    TO_DO("To do"),
+    COMPLETED("Complete")
+}
--- app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/presentation/components/ToDoTab.kt
+++ app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/presentation/components/ToDoTab.kt
@@ -1,11 +1,17 @@
 package com.example.glucode_getitdone_to_do_list.tasks.presentation.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Album
 import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
@@ -15,38 +21,44 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.navigation.ActivityNavigator
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.glucode_getitdone_to_do_list.MainActivity
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.glucode_getitdone_to_do_list.tasks.data.TaskViewModel.TaskViewModel
+import com.example.glucode_getitdone_to_do_list.tasks.data.local.Task
+import com.example.glucode_getitdone_to_do_list.tasks.data.local.TaskFilter
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ToDoTab(content: @Composable () -> Unit) {
-    val navController = rememberNavController()
-    val startDestination = Destination.ToDo
-    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+fun ToDoTab(taskViewModel: TaskViewModel,
+            onTaskTap: (Task) -> Unit,
+            onTaskLongClick: (Task) -> Unit,
+            onCheckedChanged: (Task) -> Unit
+){
+    // 2. Observe the state safely aware of the lifecycle
+    val currentFilter by taskViewModel.currentFilter.collectAsStateWithLifecycle()
+    val visibleTasks by taskViewModel.visibleTasks.collectAsStateWithLifecycle()
 
-    Scaffold {
-        Column {
+    Scaffold { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+
+
             PrimaryTabRow(
-                selectedTabIndex = selectedDestination
+                selectedTabIndex = currentFilter.ordinal
             ) {
-                Destination.entries.forEachIndexed { index, destination ->
+                TaskFilter.entries.forEach { filter ->
                     Tab(
-                        selected = selectedDestination == index,
+                        selected = currentFilter == filter,
                         onClick = {
-                            navController.navigate(route = destination.route)
-                            selectedDestination = index
+                            // Tell the ViewModel the user clicked a tab
+                            taskViewModel.updateFilter(filter)
                         },
                         text = {
                             Text(
-                                text = destination.label,
+                                text = filter.label,
                                 maxLines = 2,
                                 overflow = TextOverflow.Ellipsis
                             )
@@ -54,37 +66,43 @@ fun ToDoTab(content: @Composable () -> Unit) {
                     )
                 }
             }
-            content() //AppNavHost(navController, startDestination)
-        }
-    }
-}
-enum class Destination(
-    val route: String,
-    val label: String,
-    val icon: ImageVector,
-    val contentDescription: String
-) {
-    ToDo("toDo", "To do", Icons.Default.MusicNote, "To Do tab"),
-    Complete("complete", "Complete", Icons.Default.Album, "Complete tab")
-}
 
-@Composable
-fun AppNavHost(
-    navController: NavHostController,
-    startDestination: Destination,
-    modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController,
-        startDestination = startDestination.route
-    ) {
-        Destination.entries.forEach { destination ->
-            composable(destination.route) {
-                when (destination) {
-                    Destination.ToDo -> MainActivity()
-                    Destination.Complete -> MainActivity()
-                    else -> {
-                        MainActivity()
+            //no tasks in the visibleTasks list
+            if (visibleTasks.isEmpty()) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "There are no tasks that are ${currentFilter.label.lowercase()}",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            } else {
+            //If there are tasks, they show here
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(visibleTasks, key = { it.id }) { task ->
+                        CheckableCardComponent(
+                            title = task.title,
+                            description = task.description,
+                            isChecked = task.isComplete,
+                            onCheckedChanged = { newIsCheckedStatus ->
+                                val toggledTask = task.copy(isComplete = newIsCheckedStatus)
+                                taskViewModel.onTaskUpdated(toggledTask)
+                            },
+                            onLongClick = {
+                                onTaskLongClick(task)
+                            },
+                            onTap = {
+                                onTaskTap(task)
+                            },
+                            onEdit = {}
+                        )
                     }
                 }
             }
