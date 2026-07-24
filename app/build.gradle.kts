7fe193127d7c28f88b3b7c1492376ba4d59d4d5a
glucode-ratjatji <ratjatji@glucode.com>
2026-07-24 12:46:29 +0200

    chore: Hide API key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -5,6 +7,11 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
 android {
     namespace = "com.example.glucode_getitdone_to_do_list"
     compileSdk {
@@ -21,6 +28,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val apiKey = localProperties.getProperty("MY_API_KEY") ?: "\"\""
+        buildConfigField("String", "MY_API_KEY", apiKey)
     }
 
     buildTypes {
@@ -36,6 +46,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
--- app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/data/ytrepository/WeatherRepository.kt
+++ app/src/main/java/com/example/glucode_getitdone_to_do_list/tasks/data/ytrepository/WeatherRepository.kt
@@ -1,5 +1,6 @@
 package com.example.glucode_getitdone_to_do_list.tasks.data.ytrepository
 
+import com.example.glucode_getitdone_to_do_list.BuildConfig
 import com.example.glucode_getitdone_to_do_list.tasks.data.ytapi.WeatherApiService
 import com.example.glucode_getitdone_to_do_list.tasks.data.ytapi.WeatherResponse
 import com.example.glucode_getitdone_to_do_list.tasks.data.ytutils.Result
@@ -10,7 +11,7 @@ class WeatherRepository(private val apiService: WeatherApiService) {
     suspend fun getWeatherData(location: String): Result<WeatherResponse>{
         return try{
 val response: Response<WeatherResponse> = apiService.getCurrentWeather(
-    apiKey = "764928825f1c4aad86360913261507", //MOVEE
+    apiKey = BuildConfig.MY_API_KEY,
     location = location)
             if(response.isSuccessful){
                 val weatherResponse = response.body()
