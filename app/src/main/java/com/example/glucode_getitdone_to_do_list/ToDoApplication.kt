8b133095adf8fb01692068cbdbe6f9f6659909b8
glucode-ratjatji <ratjatji@glucode.com>
2026-08-28 15:10:23 +0200

    creating notification channel with id and name as location

--- app/src/main/java/com/example/glucode_getitdone_to_do_list/ToDoApplication.kt
+++ app/src/main/java/com/example/glucode_getitdone_to_do_list/ToDoApplication.kt
@@ -1,7 +1,27 @@
 package com.example.glucode_getitdone_to_do_list
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
+import okhttp3.Cookie
+import org.mockito.internal.matchers.Not
 
 @HiltAndroidApp
-class ToDoApplication: Application()
+class ToDoApplication: Application(){
+
+    override fun onCreate(){
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                "location",
+                "location",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
